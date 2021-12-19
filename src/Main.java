21584715213ed779efb3f966d22d16963e51127d
Maciejx7 <maciej.raulin@gmail.com>
2021-12-19 10:27:17 +0100

    zadanie 6

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,49 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        /* cw.4  */
+          /* zad.1 a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
+                Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
+                elementów i elementami mają być nazwy zwierząt.
+                b) proszę stworzyć metodę która będzie miała pętle forEach i będzie wyświetlała elementy Listy
+                -- podpowiedź: for(String zmienna : List<String>)
+                c) proszę usunąć 2 ostatnie elementy listy i dodać do niej 3 inne elementy,
+                następnie wyświetlić listę i wielkość listy,
+                d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
+                elementów listy
+          */
+        List lista1 = new ArrayList<>();
+        System.out.println("Podaj nazwe zwierzaka");
+        Scanner a = new Scanner(System.in);
+        String b =a.nextLine();
+        lista1.add(a);
 
-        /* zad.1 napisać 3 pętle for i użyc:
-        a) w pierwszej pętli break
-        b) w drugiej pętli continue
-        c) w trzeciej pętli return
-        */
-        System.out.print("liczby od 0 do 50 nie parzyste\n");
-        for( int i = 1; i <= 100;i+=2){
-            if(i==51)
-                break;
-            System.out.println(i+ " ");
 
+         /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
+                niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
+                 a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
+         */
+
+        System.out.println("Podaj 10 liczb");
+        Scanner in = new Scanner (System.in);
+        Set<Integer> liczba= new TreeSet<>();
+
+        for(int i = 0;i<10;i++){
+            int cyfra = in.nextInt();
+
+            liczba.add(cyfra);
         }
-        System.out.print("Liczby pierwsze od 1 do 100;\n");
-        int pierwsza;
-        for( int j = 1; j <= 100;j++){
-            pierwsza = 0;
-            for (int k=2; k<=j/2; k++)
-                if(j%k == 0){
-                    pierwsza = 1;
-                    continue;
-                }
-            if (pierwsza == 0)
-                System.out.print(j + " ");
-        }
-        System.out.print("Jeżeli liczba jest większa niż 11 to przerywa\n");
-        for (int i = 0; i <=100; i++){
-            if (i > 11)
-                return;
-            System.out.print(i + " ");
+        System.out.println("Liczby: "+liczba);
+        metoda(liczba);
+    }
+    public static void metoda(Set<Integer> number){
+        int i=0;
+        for(Integer x :number){
+            System.out.println("Liczba "+(i+1)+" :"+x);
+
+            i++;
         }
     }
 }
