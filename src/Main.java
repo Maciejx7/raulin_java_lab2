4f32397dc1feaff7ec9dcd4991dd80cd70f225ff
Maciejx7 <maciej.raulin@gmail.com>
2021-12-19 08:29:46 +0100

    zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* zad.1 Napisz program który będzie iterował od 100 do 0,
+         *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
+         * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
+        for (int i = 100; i >= 0; i--){
+            if(i % 4 == 0){
+                System.out.println(i);
+            }
+        }
+        /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
+         * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
+         * za pomocą pętli forEach powiększone o 11 */
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[5];
+        System.out.println("Podaj cyfrę");
+        for (int i = 0; i < 5; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i : a){
+            System.out.println(i + 11);
+        }
     }
 }
